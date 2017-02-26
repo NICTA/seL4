e8599b821e5857f30c0b8e877b0951daf1c80dd4
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-02-27 10:54:37 +1100

    Support remote modification of release queues

--- src/kernel/thread.c
+++ src/kernel/thread.c
@@ -526,7 +526,7 @@ postpone(sched_context_t *sc)
 {
     tcbSchedDequeue(sc->scTcb);
     tcbReleaseEnqueue(sc->scTcb);
-    NODE_STATE(ksReprogram) = true;
+    NODE_STATE_ON_CORE(ksReprogram, sc->scCore) = true;
 }
 
 void
